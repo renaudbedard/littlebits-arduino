26003c774c3f4d75a686a49a6d581ca04887068e
Renaud Bédard <renaud.bedard@gmail.com>
2014-07-14 01:30:54 -0400

    Rewrote part of medianAnalogRead for speed & accuracy

--- Libraries/Util/Util.cpp
+++ Libraries/Util/Util.cpp
@@ -32,13 +32,9 @@ int medianAnalogRead(byte pin)
 	static const byte AveragedBucketCount = 3;
 	static Bucket buckets[16];
 
-	byte bucketCount = 0;
-	byte mostHits = 0;
-	byte mostHitBuckets[AveragedBucketCount];
-
 	memset(&buckets, 0, sizeof buckets);
-	memset(&mostHitBuckets, 0, sizeof mostHitBuckets);
 
+	byte bucketCount = 0;
 	for (byte i = 0; i < SampleCount; i++)
 	{
 		int sample = analogRead(pin);
@@ -49,14 +45,7 @@ int medianAnalogRead(byte pin)
 			{
 				bucketFound = true;
 				buckets[j].Count++;
-				if (mostHits < buckets[j].Count)
-				{
-					for (byte k = 1; k < AveragedBucketCount; k++)
-						mostHitBuckets[k] = mostHitBuckets[k - 1];
-
-					mostHitBuckets[0] = j;
-					mostHits = buckets[j].Count;
-				}
+				break;
 			}
 
 		if (!bucketFound)
@@ -68,20 +57,35 @@ int medianAnalogRead(byte pin)
 		}
 	}
 
-	float accum = 0;
-	float weight = 0;
+	byte mostHitBuckets[AveragedBucketCount];
+	byte mostHits = 0;
+
+	memset(&mostHitBuckets, 0, sizeof mostHitBuckets);
+
+	for (byte i = 0; i < bucketCount; i++)
+		if (mostHits < buckets[i].Count)
+		{
+			for (byte j = 1; j < AveragedBucketCount; j++)
+				mostHitBuckets[j] = mostHitBuckets[j - 1];
+			mostHitBuckets[0] = i;
+			mostHits = buckets[i].Count;
+		}
+
+	int weight = 0;
+	long accum = 0;
 	for (byte i = 0; i < AveragedBucketCount; i++)
 	{
-		float thisWeight = 1.0f / (i + 1);
-		accum += (float) buckets[mostHitBuckets[i]].Value * thisWeight;
-		weight += thisWeight;
+		const Bucket& bucket = buckets[mostHitBuckets[i]];
+		accum += bucket.Value * bucket.Count;
+		weight += bucket.Count;
 	}
-	int value = (int) round(accum / weight);
+
+	int value = (int) round(accum / (float) weight);
 
 #ifdef _DEBUG
 	t = micros() - t;
 #endif
-	trace(P("sampled %i from %hhu buckets in %u ms"), value, bucketCount, (unsigned int) (t / 1000));
+	trace(P("sampled %i from %hhu buckets (%i, %i, %i) in %u ms"), value, bucketCount, buckets[mostHitBuckets[0]].Value, buckets[mostHitBuckets[1]].Value, buckets[mostHitBuckets[2]].Value, (unsigned int) (t / 1000));
 
 	return value;
 }
