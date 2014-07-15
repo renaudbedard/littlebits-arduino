e70872aa0d162c458714aaf26c2ec6c63d44849d
Renaud Bédard <renaud.bedard@gmail.com>
2014-07-14 23:34:04 -0400

    Moar tabufuckery

--- Libraries/Util/Util.h
+++ Libraries/Util/Util.h
@@ -5,18 +5,18 @@
 #include <stdio.h>
 #include <avr/pgmspace.h>
 
-#define ADD_PRINTF_SUPPORT													\
-    static FILE uartout = {0};												\
-    static int uart_putchar(char c, FILE* stream)							\
-    {																		\
-        (void) stream;														\
-        Serial.write(c);													\
-        return c;															\
-    }																		\
-    static void printf_setup()												\
-    {																		\
-        fdev_setup_stream(&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);	\
-        stdout = &uartout;													\
+#define ADD_PRINTF_SUPPORT                                                  \
+    static FILE uartout = {0};                                              \
+    static int uart_putchar(char c, FILE* stream)                           \
+    {                                                                       \
+        (void) stream;                                                      \
+        Serial.write(c);                                                    \
+        return c;                                                           \
+    }                                                                       \
+    static void printf_setup()                                              \
+    {                                                                       \
+        fdev_setup_stream(&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE); \
+        stdout = &uartout;                                                  \
     }
 
 #define P(string_literal) PSTR(string_literal "\n")
@@ -25,11 +25,11 @@
 
 #ifdef _DEBUG
 
-#define assert(cond, ...)		\
-    while (cond)				\
+#define assert(cond, ...)       \
+    while (cond)                \
         printf_P(__VA_ARGS__);
 
-#define trace(...)				\
+#define trace(...)              \
     printf_P(__VA_ARGS__)
 
 #else
